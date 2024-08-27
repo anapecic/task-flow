878b4e6cc34671f5db6fc75f4c2f638650109cb3
Ana Pecic <ana.pecic97@gmail.com>
2024-08-27 10:51:40 +0200

    button in styled component

--- components/BackButton/BackButton.js
+++ components/BackButton/BackButton.js
@@ -1,20 +1,15 @@
-import Link from "next/link";
+import styled from "styled-components";
+
+const ButtonStyle = styled.button`
+  border: 1px solid black;
+  padding: 1rem;
+  margin: 0.5rem;
+  border-radius: 50%;
+  background: grey;
+  text-decoration: none;
+  color: white;
+`;
 
 export default function BackButton({ btnContent, handleClick }) {
-  return (
-    <button
-      onClick={handleClick}
-      style={{
-        border: "1px solid black",
-        padding: "1rem",
-        margin: "0.5rem",
-        borderRadius: "50%",
-        background: "grey",
-        textDecoration: "none",
-        color: "white",
-      }}
-    >
-      {btnContent}
-    </button>
-  );
+  return <ButtonStyle onClick={handleClick}>{btnContent}</ButtonStyle>;
 }
